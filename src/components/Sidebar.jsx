f85f21d796089763d1583308e7410def84473ba7
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2025-06-16 17:37:19 +0530

    sidebar styles added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,16 @@ import {Link} from 'react-router-dom';
 
 const Sidebar=()=>{
     return(
-        <div className='bg-light border-end'>
-            <h2>CRM</h2>
-            <ul>
-                <li><Link to="/">Dashboard</Link></li>
-                <li><Link to="/contacts">Contacts</Link></li>
-                <li><Link to="/login">Login</Link></li>
+        <div className='bg-light border-end' style={{width: "250px", minHeight:"100vh"}}>
+            <div className='p-3'>
+                <h4 >CRM Menu</h4>
+            <ul className='nav flex-column' >
+                <li className='nav-item'><Link className='nav-link' to="/">Dashboard</Link></li>
+                <li className='nav-item'><Link className='nav-link'to="/contacts">Contacts</Link></li>
+                <li className='nav-item'><Link className='nav-link'to="/login">Login</Link></li>
             </ul>
+            </div>
+            
         </div>
     )
 }
