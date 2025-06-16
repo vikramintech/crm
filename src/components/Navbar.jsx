db6ef9b26d913361a1f4cff1e880990eb95b7814
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2025-06-16 17:03:34 +0530

    bootstrap installed and used utility classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import {Link} from 'react-router-dom';
 
 const Navbar=()=>{
     return(
-        <nav >
-            <h1>CRM</h1>
-            <ul>
-                <li><Link to='/'>dashboard</Link></li>
-                <li><Link to='/contacts'>Contacts</Link></li>
-                <li><Link to='/login'>login</Link></li>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4 ">
+            <Link className="navbar-brand" to='/'>CRM</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ms-auto">
+                <li className="nav-item"><Link className='nav-link' to='/' >Dashboard</Link></li>
+                <li className="nav-item"><Link className='nav-link' to='/contacts'>Contacts</Link></li>
+                <li className="nav-item"><Link className='nav-link' to='/login'>Login</Link></li>
                 
-            </ul>
+            </ul></div>
+            
         </nav>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const Sidebar=()=>{
     return(
-        <div>
+        <div className='bg-light border-end'>
             <h2>CRM</h2>
             <ul>
                 <li><Link to="/">Dashboard</Link></li>
