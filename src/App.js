a7cdc6acabb0fcdcbf5a08129da4371004521be8
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2025-06-24 19:25:19 +0530

    set private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import PrivateRoute from './routes/PrivateRoute';
+import {useAuth} from './contexts/AuthContext';
+
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Contacts from './pages/Contacts';
-import Login from './pages/Login'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App(){
+    const{currentUser}=useAuth();
     return(
         <Router>
         <Navbar/>
@@ -14,9 +19,10 @@ function App(){
         <Sidebar/>
         <div className='p-4 flex-grow-1'>
             <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/contacts' element={<Contacts/>}/>
+            <Route path='/' element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+            <Route path='/contacts' element={<PrivateRoute><Contacts/></PrivateRoute>}/>
             <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<Signup/>}/>
         </Routes>
         </div>
             
